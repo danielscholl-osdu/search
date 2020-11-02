090b60a8ab075e01ed03049fefd9a74ac0afe51b
aalekhj <aaljain@microsoft.com>
2020-11-02 15:09:47 +0530

    Minor import nits and added license header

--- provider/search-azure/src/test/java/org/opengroup/osdu/search/provider/azure/persistence/impl/RedisCacheSettingsImplTest.java
+++ provider/search-azure/src/test/java/org/opengroup/osdu/search/provider/azure/persistence/impl/RedisCacheSettingsImplTest.java
@@ -1,3 +1,17 @@
+//  Copyright © Microsoft Corporation
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 package org.opengroup.osdu.search.provider.azure.persistence.impl;
 
 import org.junit.Before;
@@ -7,12 +21,18 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.opengroup.osdu.search.provider.azure.persistence.*;
+import org.opengroup.osdu.search.provider.azure.persistence.CacheSettingDoc;
+import org.opengroup.osdu.search.provider.azure.persistence.CacheSettingSchema;
+import org.opengroup.osdu.search.provider.azure.persistence.CosmosDBCacheSettings;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RedisCacheSettingsImplTest {
@@ -20,6 +40,7 @@ public class RedisCacheSettingsImplTest {
     private final String[] ids = {"id1", "id2"};
     private final CacheSettingSchema[] cacheSettingSchemas = {getCacheSettingSchema("port1", "host1", "secret1", true), getCacheSettingSchema("port2", "host2", "secret2", false)};
     private final CacheSettingDoc[] docs = {getCacheSettingDoc(ids[0], cacheSettingSchemas[0]), getCacheSettingDoc(ids[1], cacheSettingSchemas[1])};
+
     @Mock
     private CosmosDBCacheSettings db;
 
