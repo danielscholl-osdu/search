a7c1c79f6ceeedc7eea138866d5638ff0493cdc5
Artem Dobrynin <Artem_Dobrynin@epam.com>
2020-11-10 20:10:28 +0400

    GONRG-744: Move search.Config out of os-core-common. Search, Indexer, Indexer-Queue [publish]
    https://jiraeu.epam.com/browse/GONRG-744
    
    * Changed @Value usage to @ConfigurationProperties
    * Added Search Config
    * Updated os-core-common to the version without search config

--- search-core/src/main/java/org/opengroup/osdu/search/config/SearchConfigurationProperties.java
+++ search-core/src/main/java/org/opengroup/osdu/search/config/SearchConfigurationProperties.java
@@ -9,7 +9,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "search")
 @Getter
 @Setter
 public class SearchConfigurationProperties {
