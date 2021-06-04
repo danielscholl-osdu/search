d178798caecaa49f6894dca9ae14d0372163bc4c
Rucha Deshpande <deshruch@amazon.com>
2021-06-04 14:39:00 +0000

    Bug fix: remove space
    commit 719f565a 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Fri Jun 04 2021 09:37:01 GMT-0500 (Central Daylight Time) 
    
        bug fix remove space
    
    
    commit 2ac5060d 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Fri Jun 04 2021 09:35:09 GMT-0500 (Central Daylight Time) 
    
        Merge branch 'dev' into deshruch
    
    
    commit 05065157 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Thu Jun 03 2021 14:02:49 GMT-0500 (Central Daylight Time) 
    
        bug fix
    
    
    commit e10bb09c 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Thu Jun 03 2021 13:57:29 GMT-0500 (Central Daylight Time) 
    
        read ES credentials from Secrets Manager

--- provider/search-aws/src/main/java/org/opengroup/osdu/search/provider/aws/persistence/ElasticRepositoryImpl.java
+++ provider/search-aws/src/main/java/org/opengroup/osdu/search/provider/aws/persistence/ElasticRepositoryImpl.java
@@ -55,7 +55,7 @@ public class ElasticRepositoryImpl implements IElasticRepository {
     @Value("${aws.elasticsearch.host}")
     String hostParameter;
 
-    @Value("${ aws.elasticsearch.credentials.secret}")
+    @Value("${aws.elasticsearch.credentials.secret}")
     String elasticCredentialsSecret;
 
     @Value("${aws.region}")
