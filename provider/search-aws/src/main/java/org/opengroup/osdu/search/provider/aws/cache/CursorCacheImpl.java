86e37d1df1cc7dd0fabf2e052011a9d3c308e3f9
zhijie wang <wanzhiji@amazon.com>
2021-08-26 09:45:38 -0700

    update

--- provider/search-aws/src/main/java/org/opengroup/osdu/search/provider/aws/cache/CursorCacheImpl.java
+++ provider/search-aws/src/main/java/org/opengroup/osdu/search/provider/aws/cache/CursorCacheImpl.java
@@ -57,7 +57,7 @@ public class CursorCacheImpl implements CursorCache {
      * @param INDEX_CACHE_EXPIRATION - the expiration time for the Cursor Cache Redis cluster.
      */
     public CursorCacheImpl() throws K8sParameterNotFoundException, JsonProcessingException {
-        int expTimeSeconds = Integer.parseInt(INDEX_CACHE_EXPIRATION) * 60;
+        int expTimeSeconds = 60*60;
         K8sLocalParameterProvider provider = new K8sLocalParameterProvider();
         if (provider.getLocalMode()){
             if (Boolean.parseBoolean(System.getenv("DISABLE_CACHE"))){
