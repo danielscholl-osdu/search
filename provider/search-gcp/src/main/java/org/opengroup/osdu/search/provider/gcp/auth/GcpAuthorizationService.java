eac632bf7fa03f5421fdb5b0b1d8d6fbfc3797f9
Rustam Lotsmanenko (EPAM) <rustam_lotsmanenko@epam.com>
2022-09-15 09:43:20 +0000

    Fix GCP performance issue(GONRG-5617)

---
+++ provider/search-gcp/src/main/java/org/opengroup/osdu/search/provider/gcp/auth/GcpAuthorizationService.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright 2020-2022 Google LLC
+ *  Copyright 2020-2022 EPAM Systems, Inc
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.opengroup.osdu.search.provider.gcp.auth;
+
+import com.lambdaworks.redis.RedisException;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.Nullable;
+import org.opengroup.osdu.core.common.cache.RedisCache;
+import org.opengroup.osdu.core.common.entitlements.AuthorizationServiceImpl;
+import org.opengroup.osdu.core.common.logging.JaxRsDpsLog;
+import org.opengroup.osdu.core.common.model.entitlements.AuthorizationResponse;
+import org.opengroup.osdu.core.common.model.entitlements.Groups;
+import org.opengroup.osdu.core.common.model.http.DpsHeaders;
+import org.opengroup.osdu.core.common.util.Crc32c;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Lazy
+@RequiredArgsConstructor
+public class GcpAuthorizationService extends AuthorizationServiceImpl {
+
+    private final RedisCache<String, Groups> cache;
+    private final JaxRsDpsLog jaxRsDpsLog;
+
+    @Override
+    public AuthorizationResponse authorizeAny(DpsHeaders dpsHeaders, String... roles) {
+        String cacheKey = getGroupCacheKey(dpsHeaders);
+        Groups groups = getGroupsFromCache(cacheKey);
+        if (groups == null) {
+            AuthorizationResponse authorizationResponse = super.authorizeAny(dpsHeaders, roles);
+            this.cache.put(cacheKey, authorizationResponse.getGroups());
+            return authorizationResponse;
+        } else {
+            return AuthorizationResponse.builder().user(groups.getMemberEmail()).groups(groups).build();
+        }
+    }
+
+    @Override
+    public AuthorizationResponse authorizeAny(String tenantName, DpsHeaders dpsHeaders, String... roles) {
+        String cacheKey = getGroupCacheKey(dpsHeaders);
+        Groups groups = getGroupsFromCache(cacheKey);
+        if (groups == null) {
+            AuthorizationResponse authorizationResponse = super.authorizeAny(tenantName, dpsHeaders, roles);
+            this.cache.put(cacheKey, authorizationResponse.getGroups());
+            return authorizationResponse;
+        } else {
+            return AuthorizationResponse.builder().user(groups.getMemberEmail()).groups(groups).build();
+        }
+    }
+
+    @Nullable
+    private Groups getGroupsFromCache(String cacheKey) {
+        Groups groups = null;
+        try {
+            groups = this.cache.get(cacheKey);
+        } catch (RedisException ex) {
+            this.jaxRsDpsLog.error(String.format("Error getting key %s from redis: %s", cacheKey, ex.getMessage()), ex);
+        }
+        return groups;
+    }
+
+    private String getGroupCacheKey(DpsHeaders headers) {
+        String key = String.format("entitlement-groups:%s:%s", headers.getPartitionIdWithFallbackToAccountId(),
+            headers.getAuthorization());
+        return Crc32c.hashToBase64EncodedString(key);
+    }
+}
