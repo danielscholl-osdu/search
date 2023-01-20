017b6d86bec175d217776172e313233a3578d700
Morris Estepa <estepamo@amazon.com>
2023-01-19 18:16:06 -0600

    Throw an error if the partition in search request kind is not * and not equal to the header partition id

--- provider/search-aws/src/main/java/org/opengroup/osdu/search/provider/aws/util/AwsCrossTenantUtils.java
+++ provider/search-aws/src/main/java/org/opengroup/osdu/search/provider/aws/util/AwsCrossTenantUtils.java
@@ -1,5 +1,6 @@
 package org.opengroup.osdu.search.provider.aws.util;
 
+import org.opengroup.osdu.core.common.exception.BadRequestException;
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
 import org.opengroup.osdu.search.util.CrossTenantUtils;
 import org.opengroup.osdu.core.common.search.ElasticIndexNameResolver;
@@ -18,14 +19,22 @@ public class AwsCrossTenantUtils extends CrossTenantUtils {
     @Inject
     private ElasticIndexNameResolver elasticIndexNameResolver;
 
+    // This override method forces the kind used in the index search to use the same partition id as
+    // the request's partition id header. This is to prevent data from other tenants (aka. partition)
+    // to appear when searching data in a given tenant.
     @Override
-    public String getIndexName(Query searchRequest) {
+    public String getIndexName(Query searchRequest) throws BadRequestException {
         StringBuilder builder = new StringBuilder();
         List<String> kinds = KindParser.parse(searchRequest.getKind());
         for(String kind : kinds) {
             String index = this.elasticIndexNameResolver.getIndexNameFromKind(kind);
             String[] indexArr = index.split("-");
-            indexArr[0] = dpsHeaders.getPartitionId();
+            if (indexArr[0] == "*") {
+                indexArr[0] = dpsHeaders.getPartitionId();
+            }
+            else if (indexArr[0].equalsIgnoreCase(dpsHeaders.getPartitionId()) == false) {
+                throw new BadRequestException("Invalid kind in search request.");
+            }
             builder.append(String.join("-", indexArr) + ",");
         }
         builder.append("-.*"); // Exclude Lucene/ElasticSearch internal indices
