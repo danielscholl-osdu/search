2a346ef387d7941e1514bb6b3146b63c88c00f7f
Rustam Lotsmanenko (EPAM) <rustam_lotsmanenko@epam.com>
2024-03-14 09:56:05 +0000

    search service community implementation

---
+++ search-core-plus/src/test/java/org/opengroup/osdu/search/middleware/RedirectHttpRequestsHandlerTest.java
@@ -0,0 +1,84 @@
+/*
+ *  Copyright 2020-2024 Google LLC
+ *  Copyright 2020-2024 EPAM Systems, Inc
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.opengroup.osdu.search.middleware;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.container.ContainerRequestContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.opengroup.osdu.core.common.model.http.AppException;
+import org.opengroup.osdu.search.config.SearchConfigurationProperties;
+import org.opengroup.osdu.search.service.ProviderHeaderServiceImpl;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RedirectHttpRequestsHandlerTest {
+
+  @Mock
+  ProviderHeaderServiceImpl providerHeaderService;
+  @Mock
+  private SearchConfigurationProperties searchConfigurationProperties;
+  @Mock
+  private ContainerRequestContext context;
+  @InjectMocks
+  private RedirectHttpRequestsHandler sut;
+
+  @Mock
+  private HttpServletRequest httpRequest;
+
+  @Mock
+  private HttpServletResponse httpResponse;
+
+  @Mock
+  private FilterChain filterChain;
+
+  @Test
+  public void should_throwAppException302WithHttpsLocation_when_client_isNotUsingHttps() throws Exception {
+    when(httpRequest.getScheme()).thenReturn("http");
+    try {
+      sut.doFilter(httpRequest, httpResponse, filterChain);
+      fail("should throw");
+    } catch (AppException e) {
+      assertEquals(302, e.getError().getCode());
+    }
+  }
+
+    /*
+    @Test
+    public void should_not_throwAppExceptionWithHttpsLocation_when_client_isACronJobNotUsingHttps() throws Exception {
+        Map<String,String> headers=new HashMap<>();
+        headers.put(providerHeaderService.getCronServiceHeader(),"true");
+        setupRequestHeaderMock(headers, httpRequest);
+        when(httpRequest.getScheme()).thenReturn("http");
+        when(Config.getDeploymentEnvironment()).thenReturn(DeploymentEnvironment.CLOUD);
+        try {
+        	sut.doFilter(httpRequest, httpResponse, filterChain);
+        } catch (AppException e) {
+            fail("should not throw");
+        }
+    }
+    */
+}
---
+++ search-core-plus/src/test/java/org/opengroup/osdu/search/provider/impl/CrossTenantInfoServiceTest.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright 2020-2024 Google LLC
+ *  Copyright 2020-2024 EPAM Systems, Inc
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.opengroup.osdu.search.provider.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.opengroup.osdu.core.common.model.http.DpsHeaders;
+import org.opengroup.osdu.core.common.model.tenant.TenantInfo;
+import org.opengroup.osdu.core.common.provider.interfaces.ITenantFactory;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CrossTenantInfoServiceTest {
+
+  @Mock
+  private ITenantFactory tenantFactory;
+  @Mock
+  private DpsHeaders dpsHeaders;
+  @InjectMocks
+  private CrossTenantInfoServiceImpl sut;
+
+  @Test
+  public void should_return_validTenantInfoList_given_validAccountIds() {
+    when(this.dpsHeaders.getPartitionIdWithFallbackToAccountId()).thenReturn("tenant1,common");
+    TenantInfo tenant1 = new TenantInfo();
+    tenant1.setName("tenant1");
+    when(tenantFactory.getTenantInfo("tenant1")).thenReturn(tenant1);
+    TenantInfo common = new TenantInfo();
+    common.setName("common");
+    when(tenantFactory.getTenantInfo("common")).thenReturn(common);
+    List<TenantInfo> tenantInfoList = sut.getAllTenantsFromPartitionId();
+    assertEquals(2, tenantInfoList.size());
+  }
+}
