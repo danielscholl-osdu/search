b00f1ec668b776eb1f9bc4a4a61f0214ee16d56f
ZMai <zmai@slb.com>
2024-06-11 16:32:31 -0500

    Add integration tests for queries with system/metadata kinds

--- testing/integration-tests/search-test-core/src/main/java/org/opengroup/osdu/response/ResponseMock.java
+++ testing/integration-tests/search-test-core/src/main/java/org/opengroup/osdu/response/ResponseMock.java
@@ -10,7 +10,7 @@ import java.util.Map;
 @EqualsAndHashCode(callSuper = true)
 public class ResponseMock extends ResponseBase {
     private List<Map<String, Object>> results;
-    private List<Object> aggregations;
+    private List<AggregationMock> aggregations;
     private long totalCount;
     private String cursor;
     private List<String> phraseSuggestions;
