9d3f240cc5e5385c15917829c9e358e2bd3c9ee7
Stanislav_Riabokon@epam.com <Stanislav_Riabokon@epam.com>
2024-07-31 12:22:05 +0300

    Added suggester

--- search-core/src/main/java/org/opengroup/osdu/search/util/SuggestionsQueryUtil.java
+++ search-core/src/main/java/org/opengroup/osdu/search/util/SuggestionsQueryUtil.java
@@ -3,11 +3,10 @@ package org.opengroup.osdu.search.util;
 import static org.opengroup.osdu.search.config.SearchConfigurationProperties.AUTOCOMPLETE_FEATURE_NAME;
 
 import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.CompletionSuggestOption;
 import co.elastic.clients.elasticsearch.core.search.Suggester;
-
-import java.util.*;
-
 import co.elastic.clients.elasticsearch.core.search.Suggestion;
+import java.util.*;
 import org.opengroup.osdu.core.common.feature.IFeatureFlag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,43 +19,48 @@ public class SuggestionsQueryUtil {
 
   public Suggester getSuggestions(String suggestPhrase) {
     if (!autocompleteFeatureFlag.isFeatureEnabled(AUTOCOMPLETE_FEATURE_NAME)
-        || Objects.isNull(suggestPhrase)
-        || suggestPhrase.isEmpty()) {
+            || Objects.isNull(suggestPhrase)
+            || suggestPhrase.isEmpty()) {
       return null;
     }
     return Suggester.of(
-        s ->
-            s.suggesters(
-                SUGGESTION_NAME,
-                fs ->
-                    fs.completion(c -> c.field("bagOfWords.autocomplete").skipDuplicates(true))
-                        .text(suggestPhrase)));
+            s ->
+                    s.suggesters(
+                            SUGGESTION_NAME,
+                            fs ->
+                                    fs.completion(c -> c.field("bagOfWords.autocomplete").skipDuplicates(true))
+                                            .text(suggestPhrase)));
   }
 
-      public List<String> getPhraseSuggestionsFromSearchResponse(SearchResponse<Map<String, Object>> searchResponse) {
-          if (!autocompleteFeatureFlag.isFeatureEnabled(AUTOCOMPLETE_FEATURE_NAME)) {
-            return Collections.emptyList();
-          }
+  public List<String> getPhraseSuggestionsFromSearchResponse(
+          SearchResponse<Map<String, Object>> searchResponse) {
+    if (!autocompleteFeatureFlag.isFeatureEnabled(AUTOCOMPLETE_FEATURE_NAME)) {
+      return Collections.emptyList();
+    }
 
-          var suggestBlock = searchResponse.suggest();
-          if (suggestBlock == null) {
-              return null;
-          }
-          List<String> phraseSuggestions = new ArrayList<>();
-          List<Suggestion<Map<String, Object>>> suggestions = suggestBlock.get(SUGGESTION_NAME);
+    var suggestBlock = searchResponse.suggest();
+    if (suggestBlock == null) {
+      return null;
+    }
+    List<String> phraseSuggestions = new ArrayList<>();
+    List<Suggestion<Map<String, Object>>> suggestions = suggestBlock.get(SUGGESTION_NAME);
 
-          if(suggestions == null){
-              return new ArrayList<>();
-          }
-          for(Suggestion<Map<String, Object>> suggestion : suggestions){
-          }
-
-          //        var suggestion = suggestBlock.get(SUGGESTION_NAME);
-  //        for (Entry entry : suggestion) {
-  //            for (Entry.Option option : entry) {
-  //                phraseSuggestions.add(option.getText().toString());
-  //            }
-  //        }
-          return null;
+    if (suggestions == null) {
+      return new ArrayList<>();
+    }
+    for (Suggestion<Map<String, Object>> suggestion : suggestions) {
+      if (Objects.isNull(suggestion.completion()) && suggestion.completion().options().isEmpty()) {
+        continue;
       }
+      for (CompletionSuggestOption<Map<String, Object>> completionSuggestOption :
+              suggestion.completion().options()) {
+        String text = completionSuggestOption.text();
+        if (Objects.isNull(text)) {
+          continue;
+        }
+        phraseSuggestions.add(text);
+      }
+    }
+    return phraseSuggestions;
+  }
 }
